858cd17fcc62728e15a29f7eea18c1928ff9b2d2
Татьяна Заводовская <zavodovskaiatv@hotmail.com>
2025-02-19 19:45:41 +0300

    Задание 2_2

---
+++ Task1_2_2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task1_2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int deg = Convert.ToInt32(Console.ReadLine());
+            int min = Convert.ToInt32(Console.ReadLine());
+            int sec = Convert.ToInt32(Console.ReadLine());
+            double rad = (deg * Math.PI / 180) + (min * Math.PI / (180 * 60)) + (sec * Math.PI / (180 * 60 * 60));
+            Console.WriteLine(rad);
+            Console.WriteLine();
+        }
+    }
+}
