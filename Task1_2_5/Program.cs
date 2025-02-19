e13671b1cabd0d0e28ddef4af1b6d7c8494f5279
Татьяна Заводовская <zavodovskaiatv@hotmail.com>
2025-02-19 21:30:42 +0300

    Задание 2_5

---
+++ Task1_2_5/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task1_2_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение длины трубы в метрах:");
+            double l = Convert.ToDouble(Console.ReadLine());
+            double l2 = Math.Ceiling(l);
+            Console.WriteLine("Длина трубы, округленная в большую сторону = {0}",l2);
+        }
+    }
+}
