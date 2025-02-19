03d95bb23df90496384fa242add8ef95ebad4ce7
Татьяна Заводовская <zavodovskaiatv@hotmail.com>
2025-02-19 18:58:47 +0300

    Задание 2_1

---
+++ Task1_2_1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task1_2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int t = c;
+            c = b;
+            b = a;
+            a = t;
+            Console.WriteLine("a={0},b={1},c={2}", a, b, c);
+            Console.ReadKey();
+        }
+    }
+}
